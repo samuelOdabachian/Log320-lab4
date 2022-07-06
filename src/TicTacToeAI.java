b1dc26a7ce34a3a7b910e837bf34d36169b571f8
reb-an <reb.anescar@gmail.com>
2022-07-06 16:14:44 -0400

    Impl liste des cases

--- src/TicTacToeAI.java
+++ src/TicTacToeAI.java
@@ -1,17 +1,37 @@
 import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.Vector;
 
+/**
+ * 0 -> case vide
+ * 2 -> O
+ * 4 -> X
+ */
 public class TicTacToeAI {
 
   private String _dernierCoupAdversaire;
   public TicTacToeAI() {
+    initTab();
+  }
 
+  public HashMap initTab(){
+    char[] codeCol = {'A','B', 'C', 'D',  'E', 'F', 'G', 'H', 'I'};
+    HashMap<String, Integer> listeCases = new HashMap<String, Integer>();
+    for (int i = 1; i <= 9; i++) {
+      String codeCase = Character.toString(codeCol[i-1]);
+      for (int j = 1; j <= 9; j++) {
+        listeCases.put(codeCase+j, 0);
+      }
+    }
+    System.out.println(listeCases);
+    return listeCases;
   }
 
   public String jouer() {
     System.out.println("Notre AI a joué encore. Le coup de l'adversaire était " + this._dernierCoupAdversaire);
     return "A2";
   }
-  
+
   public String jouer(String dernierCoupAdversaire) {
     this._dernierCoupAdversaire = dernierCoupAdversaire;
     System.out.println("Notre AI a joué. Le coup de l'adversaire était " + this._dernierCoupAdversaire);
